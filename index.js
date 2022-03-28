bdcc351ffcd49e4c0f221118dee8c2947f1e50d1
David Lorenz <info@activenode.de>
2022-03-28 12:14:51 +0200

    attributeChanged callback code

--- index.js
+++ index.js
@@ -12,6 +12,44 @@ class MyRecipe extends HTMLElement {
   }
 }
 
+class MyTaglist extends HTMLElement {
+  static get observedAttributes() {
+    return ["green", "warn", "info"];
+  }
+
+  constructor() {
+    super();
+  }
+
+  createTags(attributeName) {
+    const infoTags = this.getAttribute(attributeName);
+
+    if (infoTags) {
+      infoTags.split(",").forEach((tagText) => {
+        const tagDiv = document.createElement("div");
+        tagDiv.classList.add("tag", "tag-" + attributeName);
+        tagDiv.innerText = tagText;
+        this.appendChild(tagDiv);
+      });
+    }
+  }
+
+  createAllTags() {
+    this.innerHTML = "";
+    this.createTags("info");
+    this.createTags("green");
+    this.createTags("warn");
+  }
+
+  attributeChangedCallback(name, oldValue, newValue) {
+    this.createAllTags();
+  }
+}
+
 customElements.define("my-recipe", MyRecipe, {
   extends: "article",
+});
+
+customElements.define("my-taglist", MyTaglist, {
+  extends: "footer",
 });

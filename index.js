b779466756042c3aebc0e47e53175d4ed4c4d317
David Lorenz <info@activenode.de>
2022-03-28 09:03:07 +0200

    existency checks

--- index.js
+++ index.js
@@ -5,6 +5,22 @@ class MyRecipe extends HTMLElement {
   }
 }
 
-customElements.define("my-recipe", MyRecipe, {
-  extends: "article",
+customElements.whenDefined("my-recipe").then(() => {
+  console.log("my-recipe is defined now");
 });
+
+setTimeout(() => {
+  if (!customElements.get("my-recipe")) {
+    console.log("log1");
+    customElements.define("my-recipe", MyRecipe, {
+      extends: "article",
+    });
+  }
+}, 2000);
+
+// if (!customElements.get("my-recipe")) {
+//   console.log("log2");
+//   customElements.define("my-recipe", MyRecipe, {
+//     extends: "article",
+//   });
+// }

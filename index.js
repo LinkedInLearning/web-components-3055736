a0e679ecd795774a7f6b974d0f8224ffff0350ea
David Lorenz <info@activenode.de>
2022-03-28 10:06:11 +0200

    removed logs

--- index.js
+++ index.js
@@ -1,16 +1,13 @@
 class MyRecipe extends HTMLElement {
   constructor() {
     super();
-    console.log("Ich wurde instanziiert");
   }
 
   connectedCallback() {
-    console.log("i am connected");
     this.classList.add("fade-in");
   }
 
   disconnectedCallback() {
-    console.log("i am disconnected");
     this.classList.remove("fade-in");
   }
 }
